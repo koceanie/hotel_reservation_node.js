5c7323c224451933e0fa8e32688188b5d889783e
koceanie <kocean@xanny.us>
2022-07-28 18:59:53 +0900

    ADD : app.js, simple api server

--- source/app.js
+++ source/app.js
@@ -2,11 +2,53 @@ const http = require('http');
 
 const server = http.createServer();
 
+const users = [
+  {
+    id: 1,
+    name: "Rebekah Johnson",
+    email: "Glover12345@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "Fabian Predovic",
+    email: "Connell29@gmail.com",
+    password: "password",
+  },
+]
+
 const httpRequestListener = function(request, response) {
     console.log(`request object : ${request}`)
-    response.writeHead(200, {"Content-Type": "application/json"});
-    response.end(JSON.stringify({message: "Hello World!"}));
-}
+    const { url, method } = request
+    if (method === 'GET') {
+        if (url === '/api/ping') {
+            response.writeHead(200, {"Content-Type": "application/json"});
+            response.end(JSON.stringify({message: "pong pong"}));
+        }
+    } 
+    
+    else if (method === 'POST') {
+        if (url === '/api/users') {
+            let body = '';
+            console.log("body1", body)
+            request.on('data', (data) => {body += data});
+
+            request.on('end', () => {
+                console.log("body2", body)
+                const user = JSON.parse(body);
+
+                users.push({
+                    id : user.id,
+                    name : user.name,
+                    email : user.email,
+                    password : user.password
+                })
+                console.log(users)
+            response.end(JSON.stringify({message : 'ADDED'}));
+            })
+        }
+    }
+};
 
 server.on("request", httpRequestListener);
 
